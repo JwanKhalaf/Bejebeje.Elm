055d55fcc1647786d1c75307db889e0f2f3ddaeb
J Khalaf <jwan.khalaf1@nhs.net>
2019-12-12 23:55:18 +0000

    add the bejebeje logo

--- src/app/Main.elm
+++ src/app/Main.elm
@@ -1,15 +1,82 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Browser exposing (application)
+import Browser.Navigation as Nav
+import Html exposing (h1, span, text)
+import Html.Attributes exposing (class)
+import Url
 
 
-n =
-    "Hello world!"
-
-
+main : Program () Model Msg
 main =
-    let
-        _ =
-            Debug.log "number" n
-    in
-    text n
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( Model key url, Cmd.none )
+
+
+
+-- Update
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "URL Interceptor"
+    , body =
+        [ h1 [ class "logo" ] [ span [ class "logo__letter" ] [ text "B" ], text "êjebêje" ]
+        ]
+    }
