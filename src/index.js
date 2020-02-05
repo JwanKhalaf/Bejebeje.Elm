bf20c5500d73cf526ea9ebe7d2395d8695cd75a7
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-02-05 22:30:29 +0000

    replace 100vh for 100% in height

--- src/index.js
+++ src/index.js
@@ -11,9 +11,3 @@ const app = Elm.Main.init({
   node: document.getElementById("elm-root"),
   flags: { apiRootUrl: API_ROOT_URI }
 });
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty("--vh", `${vh}px`);
